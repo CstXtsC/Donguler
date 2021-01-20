8616ed6608f141b277753e435690c91ef1362243
CstXtsC <medemir80@gmail.com>
2021-01-20 23:56:29 +0300

    16 Ocak ders sonu

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ?? --> 30 doğru yaptım
+            Console.WriteLine(sayi1);
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };   // bu bir array(dizi)dir.
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] ?? --> 999 doğru yaptım
+            Console.WriteLine(sayilar1[0]);
+
+
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kampı", "Programlamaya başlangıç için temel kurs", "Java", "Python", "C#" };
+
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("FOR BİTTİ");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
